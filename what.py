1192a3e52dc9a4f75799d884613dc1b5e104cab3
yillen zhu <yillenzhu@yillens-MacBook-Air.local>
2016-05-21 23:14:53 +0800

    Following Links in Python

--- what.py
+++ what.py
@@ -1,15 +1,15 @@
 import urllib
 from BeautifulSoup import *
 
-url = raw_input('Enter - ')
-html = urllib.urlopen(url).read()
+url = raw_input('Enter URL: ')
+count = raw_input('Enter count: ')
+position = raw_input('Enter position: ')
 
-soup = BeautifulSoup(html)
-
-tags = soup('span')
-
-print sum([int(tag.contents[0]) for tag in tags])
-#sum = 0
-#for tag in tags:
-    #sum = sum + int(tag.contents[0])
-#print sum 
+while count >= 0:
+    print 'Retrieving: ', url
+    count = int(count) - 1
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    tags = soup('a')
+    url = tags[int(position) - 1].get('href', None)
+    #print 'Retrieving:', url
